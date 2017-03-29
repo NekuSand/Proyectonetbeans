aa2cb7d9c02c8a0fdaa22193b537467ac6eda669
NekuSand <nekusand@gmail.com>
2017-03-29 11:19:45 +0200

    Update JavaApplication6pruebaCODE.java
    pequeño cambio de modo prueba para verlo

--- src/javaapplication6pruebacode/JavaApplication6pruebaCODE.java
+++ src/javaapplication6pruebacode/JavaApplication6pruebaCODE.java
@@ -17,6 +17,7 @@ public class JavaApplication6pruebaCODE {
     public static void main(String[] args) {
         // TODO code application logic here
         int i = 0; 
+        int j= 5;
     }
     
 }
